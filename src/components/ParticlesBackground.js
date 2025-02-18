9c94a1c751135bbd25e688e9c91cd5dcf5acbf7a
Rahul Kintali <72811031+RahulKintali@users.noreply.github.com>
2025-02-17 20:24:37 -0600

    Update ParticlesBackground.js

--- src/components/ParticlesBackground.js
+++ src/components/ParticlesBackground.js
@@ -1,15 +1,72 @@
+import { useCallback } from "react";
 import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 const ParticlesBackground = () => {
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
+
   return (
     <Particles
+      id="tsparticles"
+      init={particlesInit}
       options={{
-        background: { color: "#ffffff" },
+        background: {
+          color: "#ffffff",
+        },
+        fpsLimit: 60,
+        interactivity: {
+          events: {
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+          },
+          modes: {
+            repulse: {
+              distance: 150,
+              duration: 0.4,
+            },
+          },
+        },
         particles: {
-          number: { value: 50 },
-          size: { value: 3 },
-          move: { speed: 2 },
-          links: { enable: true, distance: 150, color: "#000000" },
+          number: {
+            value: 50,
+            density: {
+              enable: true,
+              area: 800,
+            },
+          },
+          color: {
+            value: "#000000",
+          },
+          shape: {
+            type: "circle",
+          },
+          opacity: {
+            value: 0.5,
+            random: true,
+          },
+          size: {
+            value: 4,
+            random: true,
+          },
+          move: {
+            enable: true,
+            speed: 2,
+            direction: "none",
+            random: false,
+            straight: false,
+            outModes: "out",
+          },
+          links: {
+            enable: true,
+            distance: 150,
+            color: "#000000",
+            opacity: 0.4,
+            width: 1,
+          },
         },
       }}
       className="absolute w-full h-full -z-10"
