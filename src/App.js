5318a4becc0509a5ccd66a5d5f91c419fbffbe91
Rahul Kintali <72811031+RahulKintali@users.noreply.github.com>
2025-02-17 20:12:48 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Home from "./components/Home";
+import Education from "./components/Education";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
 
 function App() {
   return (
     <Router>
       <div className="flex">
         <Sidebar />
-        <div className="ml-0 md:ml-64 w-full">
+        <div className="ml-0 md:ml-64 w-full p-6">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/education" element={<Education />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/experience" element={<Experience />} />
           </Routes>
         </div>
       </div>
